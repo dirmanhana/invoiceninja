ce4a49e9465e397ce07b239331a6c9c8b1da4a62
dirman <35218057+dirmanhana@users.noreply.github.com>
2025-06-23 22:26:03 +0700

    Create help.php
    add indonesian language
    
    Signed-off-by: dirman <35218057+dirmanhana@users.noreply.github.com>

---
+++ lang/id/help.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang = array(
+    'client_dashboard' => 'Pesan yang ditampilkan di dasbor klien',
+    'client_currency' => 'Mata uang klien.',
+    'client_language' => 'Bahasa klien.',
+    'client_payment_terms' => 'Syarat pembayaran klien.',
+    'client_paid_invoice' => 'Pesan yang ditampilkan di layar faktur lunas klien',
+    'client_unpaid_invoice' => 'Pesan yang ditampilkan di layar faktur belum dibayar klien',
+    'client_unapproved_quote' => 'Pesan yang ditampilkan di layar penawaran yang belum disetujui klien',
+);
+
+return $lang;
