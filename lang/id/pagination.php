eaffd3aa958f7c1e0488dbd1343d6039d8279e3d
dirman <35218057+dirmanhana@users.noreply.github.com>
2025-06-23 22:27:09 +0700

    Create pagination.php
    add indonesian language
    
    Signed-off-by: dirman <35218057+dirmanhana@users.noreply.github.com>

---
+++ lang/id/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa untuk Paginasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh pustaka paginasi untuk membangun
+    | tautan paginasi sederhana. Anda bebas mengubahnya sesuai kebutuhan
+    | agar tampilan lebih sesuai dengan aplikasi Anda.
+    |
+    */
+
+    'previous' => '« Sebelumnya',
+    'next' => 'Berikutnya »',
+
+];
