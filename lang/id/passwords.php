2b71d2d6d89b846d6793ea8070d5714b9604ad5f
dirman <35218057+dirmanhana@users.noreply.github.com>
2025-06-23 22:28:13 +0700

    Create passwords.php
    add indonesian language
    
    Signed-off-by: dirman <35218057+dirmanhana@users.noreply.github.com>

---
+++ lang/id/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa untuk Atur Ulang Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah pesan bawaan yang sesuai dengan alasan
+    | kegagalan saat mencoba memperbarui kata sandi, seperti token yang tidak
+    | valid atau kata sandi baru yang tidak sesuai.
+    |
+    */
+
+    'password' => 'Kata sandi harus terdiri dari minimal enam karakter dan cocok dengan konfirmasi.',
+    'reset' => 'Kata sandi Anda telah berhasil diatur ulang!',
+    'sent' => 'Kami telah mengirimkan tautan untuk mengatur ulang kata sandi Anda melalui email!',
+    'token' => 'Token pengaturan ulang kata sandi ini tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+    'throttled' => 'Anda baru saja meminta pengaturan ulang kata sandi, silakan periksa email Anda.',
+
+];
